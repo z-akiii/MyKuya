80cbab35df187d44f126d76eddbd92d49dff19b6
brammms <acsicat1@student.hau.edu.ph>
2025-08-31 15:24:02 +0800

    Update delivery_page.dart

--- lib/screens/delivery_page.dart
+++ lib/screens/delivery_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class DeliveryPage extends StatelessWidget {
   const DeliveryPage({super.key});
-
+//
   @override
   Widget build(BuildContext context) {
     return GestureDetector(

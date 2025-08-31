4e322a5c982ec686f38596c4711fdf5ba14b7b64
brammms <acsicat1@student.hau.edu.ph>
2025-08-31 15:37:22 +0800

    Update shopping_page.dart

--- lib/screens/shopping_page.dart
+++ lib/screens/shopping_page.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 
+// Shopping Page
 class ShoppingPage extends StatelessWidget {
   const ShoppingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // App bar
       appBar: AppBar(
         title: Text('Shopping'),
         centerTitle: true,
         backgroundColor: Colors.white,
-        ),
-        backgroundColor: Colors.white,
-        body: Center(
-          child: Text('Coming Soon...'),
-        ),
+      ),
+      backgroundColor: Colors.white,
+      // Placeholder Context
+      body: Center(
+        child: Text('Coming Soon...'),
+      ),
     );
   }
 }

c22b8466a91791c2a7594714ba7123368f321e62
brammms <acsicat1@student.hau.edu.ph>
2025-08-31 15:38:23 +0800

    Update pet_page.dart

--- lib/screens/pet_page.dart
+++ lib/screens/pet_page.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 
+// Pet Sitting screen
 class PetPage extends StatelessWidget {
   const PetPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // App bar
       appBar: AppBar(
         title: Text('Pet Sitting'),
         centerTitle: true,
         backgroundColor: Colors.white,
       ),
       backgroundColor: Colors.white,
+      // Placeholder content
       body: Center(
         child: Text('Coming soon...'),
       ),

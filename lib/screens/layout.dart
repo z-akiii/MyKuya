f40fa5efa5481275a9ebe3c816b1af93e22f9588
brammms <acsicat1@student.hau.edu.ph>
2025-08-31 15:41:00 +0800

    Update layout.dart

--- lib/screens/layout.dart
+++ lib/screens/layout.dart
@@ -3,6 +3,7 @@ import 'package:mykuya/screens/home_page.dart';
 import 'package:mykuya/screens/notifications.dart';
 import 'package:mykuya/screens/wallet.dart';
 
+// Main layout widget with bottom navigation
 class Layout extends StatefulWidget {
   const Layout({super.key});
 
@@ -11,19 +12,21 @@ class Layout extends StatefulWidget {
 }
 
 class _LayoutState extends State<Layout> {
-
+  // Keeps track of the current screen index
   int _currentScreen = 0;
 
+  // Updates the screen when a navigation item is tapped
   void _screenNavigator(int index) {
     setState(() {
       _currentScreen = index;
     });
   }
 
+  // List of screens for navigation
   final List _screens = [
-  HomePage(),
-  Notifications(),
-  Wallet(),
+    HomePage(),
+    Notifications(),
+    Wallet(),
   ];
 
   @override
@@ -31,15 +34,18 @@ class _LayoutState extends State<Layout> {
     return Scaffold(
       backgroundColor: Colors.white,
 
+      // Displays the current screen
       body: _screens[_currentScreen],
 
+      // Bottom navigation bar
       bottomNavigationBar: Container(
         decoration: BoxDecoration(
           border: Border(
             top: BorderSide(
               color: const Color.fromRGBO(207, 216, 220, 1),
-              width: .5)
-          )
+              width: .5,
+            ),
+          ),
         ),
 
         child: BottomNavigationBar(
@@ -51,25 +57,25 @@ class _LayoutState extends State<Layout> {
           currentIndex: _currentScreen,
           onTap: _screenNavigator,
           items: [
-        
+            // Home item
             BottomNavigationBarItem(
-              icon: Icon(Icons.home,
-              color: Color(0xFF55A2F0)),
-              label: 'Home'
-              ),
-        
+              icon: Icon(Icons.home, color: Color(0xFF55A2F0)),
+              label: 'Home',
+            ),
+
+            // Notifications item
             BottomNavigationBarItem(
-              icon: Icon(Icons.notifications,
-              color: Color(0xFF55A2F0)),
-              label: 'Notifications'),
-        
+              icon: Icon(Icons.notifications, color: Color(0xFF55A2F0)),
+              label: 'Notifications',
+            ),
+
+            // Wallet item
             BottomNavigationBarItem(
-              icon: Icon(Icons.wallet,
-              color: Color(0xFF55A2F0)),
-              label: 'Wallet')
-        
-          ]
-          ),
+              icon: Icon(Icons.wallet, color: Color(0xFF55A2F0)),
+              label: 'Wallet',
+            ),
+          ],
+        ),
       ),
     );
   }

a48bec642338be454c66f8b201b9d625cc0388ad
brammms <acsicat1@student.hau.edu.ph>
2025-08-31 15:05:56 +0800

    Update notifications.dart

--- lib/screens/notifications.dart
+++ lib/screens/notifications.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
+// Notifications screen
 class Notifications extends StatelessWidget {
   const Notifications({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // Simple placeholder text
       body: Center(child: Text('Notifications')),
     );
   }
